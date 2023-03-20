d4a3b2f210605ed85490b097a88d0b78d677c54f
Mormas <657560745@qq.com>
2023-03-20 09:24:06 +0800

    新增下载模板：基于vue3的demo模板

--- config/choice.js
+++ config/choice.js
@@ -3,4 +3,8 @@ export default [
         name: '基础配置',
         value: 'repo-basic',
     },
+    {
+        name: 'vue3 初始化模板（用于搭建demo）',
+        value: 'vue-demo',
+    },
 ];
--- config/template.js
+++ config/template.js
@@ -1,3 +1,4 @@
 export default {
     'repo-basic': 'https://github.com:MormasMingo/gracem-template-basic',
+    'vue-demo': 'https://github.com:MormasMingo/gracem-template-vue3#demo',
 };
